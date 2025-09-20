a58b6d569ea799df9513a0fdd89c637a9078a327
Derek Honerlaw <honerlawd@gmail.com>
2025-09-20 18:14:49 -0400

    conditional apiUrl

--- packages/app/app.config.ts
+++ packages/app/app.config.ts
@@ -1,13 +1,14 @@
 import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => {
+    const isProduction = process.env.APP_VARIANT === "production" || process.env.NODE_ENV === "production";
     return {
         ...config,
         name: "Verena",
         slug: "verena",
         extra: {
             ...config.extra,
-            apiUrl: "http://localhost:3000" // "https://www.verena.app"
+            apiUrl: isProduction ? "https://www.verena.app" : "http://localhost:3000"
         }
     }
 }
